dec276b5ab50c153618a1a09699648ee20521be1
Mykoka Gumenyak <m.gumenyak@gmail.com>
2016-02-13 23:40:21 +0200

    first commit

---
+++ Release/7segment4digitsDisplay.d
@@ -0,0 +1,6 @@
+7segment4digitsDisplay.o: ../7segment4digitsDisplay.c ../uart.h \
+ ../7segment4digitsDisplay.h
+
+../uart.h:
+
+../7segment4digitsDisplay.h:
---
+++ Release/main.d
@@ -0,0 +1,5 @@
+main.o: ../main.c ../uart.h ../7segment4digitsDisplay.h
+
+../uart.h:
+
+../7segment4digitsDisplay.h:
